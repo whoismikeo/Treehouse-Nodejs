e8a3cb43808e4a257de1b0baf65d8162812ccfab
Mike O <whoismikeo@gmail.com>
2019-12-30 10:43:59 -0800

    api request works

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-console.log("Hello world");
-console.error("OHHH NOESS");
-console.dir({name: "Mike", age: 21, });
+// https://teamtreehouse.com/whoismikeo.json
+// access the json from Treehouse api and print
+
+// Require https
+const username = 'whoismikeo'
+var https = require("https");
+// Function to print message to console
+function printMessage(username, badgeCount, point) {
+    const message = `${username} has ${badgeCount} badges ${points} points in JavaScript.`
+    console.log(message)
+}
+
+// API request
+const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+    // Read the data
+    console.dir(response)
+    // Parse the data
+
+    // Print the data
+
+});
+
